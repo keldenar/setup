2c7befd1b67ceba2e6797368417d2a99e1c31bb4
Bret Combast <bret_combast@ultimatesoftware.com>
2017-03-14 09:49:00 -0400

    Added script to install ansible.

---
+++ run_first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~
+git clone https://github.com/ansible/ansible.git
+cd ansible
+git checkout stable-2.2
+git submodule init
+git submodule update
+
+
+echo "Remember to source ~/ansible/hacking/env-setup before continuing!"
+
